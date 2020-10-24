b0437201933daaf02e29aafaaadc51d4098f6fe6
AnaMariaBiliciuc <70842937+AnaMariaBiliciuc@users.noreply.github.com>
2020-10-24 18:56:29 +0300

    Add files via upload

---
+++ w2.py
@@ -0,0 +1,21 @@
+n=0
+sp=0
+sn=0
+np=0
+nn=0
+c=0
+while c<12:
+    n=eval(input('Introduceti temperatura:'))
+    if n<55 and n>-60:
+        c+=1
+        if n>0:
+            sp+=n
+            np+=1
+        elif n<0:
+            sn+=n
+            nn+=1 
+             
+if np>0:
+    print('medie_poz=',round(float(sp/np), 2))
+if nn>0:
+    print("medie_neg=",round(float(sn/nn), 2))
